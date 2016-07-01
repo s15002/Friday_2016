13b0be14a0ff47af9dfe9071fd582bfe8d5b3f18
s15002 <s15002@std.it-college.ac.jp>
2016-07-01 12:19:46 +0900

    問題追加

---
+++ Training/1001.py
@@ -0,0 +1,28 @@
+#与えられた2つのパラメーターの合計を2倍したものが60を超えているかどうか
+def check_sum_2times_over_60(par1, par2):
+    pass
+
+
+#与えられた金額に消費税率8%を含めた値が5000を超えているかどうか
+def tax_include(cost):
+    pass
+
+
+#与えられたスコアを80以上なら'A'、60以上80未満なら'B'、45以上60未満なら'C'、
+#45未満は'F'と返す
+def judge_rank(score):
+    pass
+
+
+#与えられた数値の階乗を返す。ただし再帰は使用禁止。
+def factorial(num):
+    pass
+
+
+#与えられた数値の2の累乗を返す。再帰使用禁止。便利な演算子使用禁止。
+def power_of_two(num):
+    pass
+
+
+if __name__ == '__main__':
+    pass
